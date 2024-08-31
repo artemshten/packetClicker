bfd95752bde4985135db256d9ada724d1490fdbb
artemshten <artemshten@yandex.ru>
2024-08-31 14:41:46 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,62 @@
+import flet as ft
+from asyncio import sleep
+from pytonconnect import TonConnect
+
+async def main(page: ft.Page):
+    page.title = 'Packet Cliker'
+    page.theme_mode = ft.ThemeMode.DARK
+    page.bgcolor = '#000000'
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.fonts = {"Calibri" : "fonts/calibribold.ttf"}
+    page.theme = ft.Theme(font_family="Calibri")
+
+    async def score_up(event: ft.ContainerTapEvent):
+        score.data += 1
+        score.value = str(score.data)
+
+        image.scale = 0.95
+
+        score_counter.opacity = 0
+        score_counter.value = '+1'
+        score_counter.right = 0
+        score_counter.left = 150
+        score_counter.top = 0
+        score_counter.bottom = 0
+
+        progress_bar.value += (1/100)
+
+        if score.data % 100 == 0:
+            page.snack_bar = ft.SnackBar(content=ft.Text(value='+100',
+                                                         size=20,
+                                                         color='#ffffff',
+                                                         text_align=ft.TextAlign.CENTER), bgcolor='#cccccc')
+            page.snack_bar.open = True
+            progress_bar.value = 0
+
+        await page.update_async()
+        await sleep(0.1)
+        image.scale = 1
+        score_counter.opacity = 0
+        await page.update_async()
+
+    score = ft.Text(value='0', size=70, data=0)
+    score_counter = ft.Text(size=50, animate_opacity=ft.Animation(duration=200, curve=ft.AnimationCurve.BOUNCE_IN))
+    image = ft.Image(src='packet.png',
+                     fit=ft.ImageFit.CONTAIN,
+                     animate_scale=ft.Animation(duration=200, curve=ft.AnimationCurve.EASE))
+
+    progress_bar = ft.ProgressBar(value=0,
+                                  width=page.width-100,
+                                  height=20,
+                                  color='#ffffff',
+                                  bgcolor='#cccccc'
+                                  )
+
+    await page.add_async(score,
+                         ft.Container(content=ft.Stack(controls=[image, score_counter]), on_click=score_up, margin=ft.Margin(0,0,0,30)),
+                         ft.Container(content=progress_bar, border_radius=ft.BorderRadius(10,10,10,10))
+                         )
+
+if __name__ == '__main__':
+    ft.app(target=main, view=None, port=8000)
---
+++ bot.py
@@ -0,0 +1,35 @@
+import asyncio
+from aiogram import Bot, Dispatcher, Router, F
+from aiogram.types import Message, CallbackQuery, WebAppInfo
+from aiogram.filters import CommandStart
+from aiogram.enums import ParseMode
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+
+def webapp_builder():
+    builder = InlineKeyboardBuilder()
+    builder.button(text='⚪️ Открыть игру', web_app=WebAppInfo(url='https://7325-109-237-12-246.ngrok-free.app'))
+    return builder.as_markup()
+
+
+router = Router()
+
+
+@router.message(CommandStart())
+async def start(message: Message):
+    await message.reply('Привет!\nЛистинг уже не за горами, так что заходи и кликай пакеты',
+                        reply_markup=webapp_builder())
+
+
+async def main():
+    bot = Bot('7528949580:AAE2tCNpzwqAEokJ44uXi4i4-EleVlhTePM')
+
+    dp = Dispatcher()
+    dp.include_router(router)
+
+    await bot.delete_webhook(True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
